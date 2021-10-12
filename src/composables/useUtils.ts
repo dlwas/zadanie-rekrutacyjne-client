6b3cc730f56ca5d0ea125d9bd6c4dd4d0856c1ab
dlwas <57624006+dlwas@users.noreply.github.com>
2021-10-12 19:03:32 +0200

    add "focusById"

--- src/composables/useUtils.ts
+++ src/composables/useUtils.ts
@@ -1,3 +1,24 @@
 export const doError = (err: string) => {
   throw new Error(err)
 }
+
+export const focusById = (element: string, mode: string): void => {
+  const modes = ['focus', 'unfocus']
+  if (element && mode) {
+    if (modes.includes(mode)) {
+      const elm = document.getElementById(element)
+      if (elm) {
+        if (mode === 'focus') elm.focus()
+        if (mode === 'unfocus') elm.blur()
+      } else {
+        doError('Element not found, provide a real id name')
+      }
+    } else {
+      doError(`Invalid "mode" name, expect: ${modes}`)
+    }
+  } else {
+    doError(
+      `Expect: "element": string, "mode": string , got: "element":${typeof element}, "mode":${typeof mode}`
+    )
+  }
+}
