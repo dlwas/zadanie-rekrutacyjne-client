f54d010d9c8872496466a97318ab61be03d24d7f
dlwas <57624006+dlwas@users.noreply.github.com>
2021-10-12 19:05:52 +0200

    Update Filters components

--- src/components/Filters.vue
+++ src/components/Filters.vue
@@ -1,16 +1,93 @@
 <template>
-  <div>Filters</div>
+  <div class="w-full h-auto my-3">
+    <div class="text-right">
+      <span @click="toggleFilters" class="cursor-pointer">FILTRY I KATEGORIE</span>
+    </div>
+    <div v-if="isExpanded" class="flex flex-wrap md:space-x-10">
+      <FiltersItem label="Domki:" :data="filters.house" />
+      <FiltersItem label="Zwierzęta:" :data="filters.animals" />
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref, watch } from 'vue'
+import FiltersItem from './FiltersItem.vue'
+import useBreakpoints from '../composables/useBreakpoints'
 
 export default defineComponent({
   name: 'Filters',
-  components: {},
+  components: { FiltersItem },
   setup() {
+    const { windowWidth } = useBreakpoints()
+    const isExpanded = ref(windowWidth.value >= 768)
 
-    return {}
+    const toggleFilters = () => {
+      isExpanded.value = isExpanded.value == false ? true : false
+    }
+
+    watch(windowWidth, () => {
+      isExpanded.value = windowWidth.value >= 768
+    })
+
+    const filters = ref({
+      house: [
+        {
+          name: 'Wszystkie',
+          default: true,
+          value: null,
+        },
+        {
+          name: 'Na drzewie',
+          default: false,
+          value: 1,
+        },
+        {
+          name: 'Na ziemi',
+          default: false,
+          value: 1,
+        },
+      ],
+      animals: [
+        {
+          name: 'Koty',
+          default: false,
+          value: 1,
+        },
+        {
+          name: 'Psy',
+          default: false,
+          value: 1,
+        },
+        {
+          name: 'Konie',
+          default: false,
+          value: 1,
+        },
+        {
+          name: 'Ptaki',
+          default: false,
+          value: 1,
+        },
+        {
+          name: 'Jeże',
+          default: false,
+          value: 1,
+        },
+        {
+          name: 'Króliki',
+          default: false,
+          value: 1,
+        },
+        {
+          name: 'Kury',
+          default: false,
+          value: 1,
+        },
+      ],
+    })
+
+    return { isExpanded, toggleFilters, filters }
   },
 })
 </script>
