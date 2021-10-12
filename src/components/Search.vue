0fc55f1e4a9ea479d20bae301c2e7d98a2ae038a
dlwas <57624006+dlwas@users.noreply.github.com>
2021-10-12 19:39:33 +0200

    Update Search.vue
    fix styles

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -4,7 +4,7 @@
     type="text"
     placeholder="(Tab) Szukaj..."
     ref="inputSearch"
-    class="p-1 pl-2" />
+    class="p-1 pl-2 border border-gray-300 rounded" />
 </template>
 
 <script lang="ts">
