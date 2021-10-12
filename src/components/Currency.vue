02694398846db51ec90b1f49916f44753af1d56c
dlwas <57624006+dlwas@users.noreply.github.com>
2021-10-12 19:39:05 +0200

    Create Currency.vue

---
+++ src/components/Currency.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>Currency</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Currency',
+  components: {},
+  setup() {
+
+    return {}
+  },
+})
+</script>
